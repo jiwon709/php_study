88665551e7ac8aa7bf03cd3dda0da950d9e165cf
jiwon709 <jimong8702@naver.com>
2022-02-20 23:13:06 +0900

    conditional, comparison

---
+++ comparison.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <h1>Comparison Operators &amp;
+        Boolean data type</h1>
+    
+    <?php
+    var_dump(2);
+    var_dump(1==1);
+    var_dump(1==2);
+    var_dump(1>=1);
+    var_dump(1>1);
+    ?>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,20 +4,43 @@
     <title></title>
 <body>
 </body>
-    <h1>WEB</h1>
+    <h1><a href="index.php">WEB</a></h1>
     <ol>
-        <li><a href="index.php?id=HTML">HTML</a></li>
+
+        <?php
+        $list = scandir('data');
+        $i = 0;
+        while($i<count($list)){
+            if($list[$i] != '.'){
+                if($list[$i] != '..'){
+                    ?>
+                    <li><a href="index.php?id=<?=$list[$i]?>"><?=$list[$i]?></a></li>
+                    <?php
+                }
+            }
+            $i = $i + 1;
+        }
+        ?>
+        <!-- <li><a href="index.php?id=HTML">HTML</a></li>
         <li><a href="index.php?id=CSS">CSS</a></li>
-        <li><a href="index.php?id=JavaScript">JavaScriptL</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScriptL</a></li> -->
     </ol>
     <h2>
         <?php
+        if(isset($_GET['id'])){
             echo $_GET['id'];
+        } else {
+            echo "Welcome";
+        }
         ?>
     </h2>
 
     <?php
+    if(isset($_GET['id'])){
     echo file_get_contents("data/".$_GET['id']);
+    } else {
+        echo "Hello, PHP";
+    }
     ?>
     
 </head>
